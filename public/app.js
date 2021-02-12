4f61d87e4a085e8e5d6a7c342ff6ac07439aefbb
vinayak dubey <vinayak.dubey2019@vitstudent.ac.in>
2021-02-13 04:02:36 +0530

    we have DB now gois

--- public/app.js
+++ public/app.js
@@ -1,5 +1,13 @@
 document.addEventListener("DOMContentLoaded", event => {
     const app = firebase.app()
+    const db = firebase.firestore()
+    const coord = db.collection("coordinates").doc("JAozRMxXH8p0SA3IecdN")
+    coord.get()
+        .then(doc => {
+            const data = doc.data();
+            document.write(data.cow_where.N_ + `<br>` + data.cow_where.x_)
+            console.log(data)
+        })
 })
 
 function googleLogin(){
