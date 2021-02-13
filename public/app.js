79db5963f8852f77e8256fefe137e1d7b7bc7218
vinayak dubey <vinayak.dubey2019@vitstudent.ac.in>
2021-02-13 05:48:35 +0530

    function that checks if cow is out is ready bois

--- public/app.js
+++ public/app.js
@@ -1,13 +1,6 @@
 document.addEventListener("DOMContentLoaded", event => {
     const app = firebase.app()
-    const db = firebase.firestore()
-    const coord = db.collection("coordinates").doc("JAozRMxXH8p0SA3IecdN")
-    coord.get()
-        .then(doc => {
-            const data = doc.data();
-            document.write(data.cow_where.N_ + `<br>` + data.cow_where.x_)
-            console.log(data)
-        })
+    setInterval(isOut,1000)
 })
 
 function googleLogin(){
@@ -18,4 +11,23 @@ function googleLogin(){
             document.write(`hello ${user.displayName}`)
         })
         .catch(console.log)
+}
+
+function isOut(){
+    const boundary = {min_lat:0, max_lat:10, min_long:0, max_long:10}
+    const db = firebase.firestore()
+    const coord = db.collection("coordinates").doc("JAozRMxXH8p0SA3IecdN")
+    coord.get()
+    .then(doc => {
+        const data = doc.data();
+        document.write(data.cow_where.N_ + `<br>` + data.cow_where.x_)
+        if((boundary.min_lat<=data.cow_where.N_ && data.cow_where.N_<=boundary.max_lat) && (boundary.min_long<=data.cow_where.x_ && data.cow_where.x_<=boundary.max_long)){
+            document.write('<br>YOUR COW IS SAFE')
+            return false
+        }
+        else{
+            document.write("<br>YOUR COW IS OUT")
+            return true
+        }
+    })
 }
